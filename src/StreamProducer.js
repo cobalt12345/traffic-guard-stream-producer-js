fbed44e7e88d28f7d4320723c43551a6abd44eeb
dtalochk <denis.talochkin@gmail.com>
2021-04-23 18:59:13 +0300

    Add JS.

--- src/StreamProducer.js
+++ src/StreamProducer.js
@@ -1,11 +1,20 @@
+// Initialize the Amazon Cognito credentials provider
+// AWS.config.region = 'eu-central-1'; // Region
+// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//     IdentityPoolId: 'eu-central-1:7779208d-624e-4741-9f76-42a18bbd9cae',
+// });
+const { CognitoIdentityClient } = require("@aws-sdk/client-cognito-identity");
+const {
+    fromCognitoIdentityPool,
+} = require("@aws-sdk/credential-provider-cognito-identity");
 import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
-
+const REGION = 'eu-central-1';
 let s3ClientConfig = {
-    region: 'eu-central-1',
-    credentials: {
-        AccessKeyId: 'AKIAW2W76X6JUKKSZR5F1',
-        SecretAccessKey: 'sacclyK+wZt8VYHEoo5jaQaX4K5mdfoR8+2s99jw1'
-    }
+    region: REGION,
+    credentials: fromCognitoIdentityPool({
+        client: new CognitoIdentityClient({ region: REGION }),
+        identityPoolId: 'eu-central-1:7779208d-624e-4741-9f76-42a18bbd9cae',
+    }),
 };
 const client = new S3Client(s3ClientConfig);
 let listBucketsCommand = new ListBucketsCommand({});
