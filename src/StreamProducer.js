285c064469bbd9d9ccde10646266ba40436a3c53
Denis Talochkin <vgtu@rambler.ru>
2021-04-28 15:24:04 +0300

    gtrgtr

--- src/StreamProducer.js
+++ src/StreamProducer.js
@@ -3,39 +3,64 @@
 // AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 //     IdentityPoolId: 'eu-central-1:7779208d-624e-4741-9f76-42a18bbd9cae',
 // });
-const { CognitoIdentityClient } = require("@aws-sdk/client-cognito-identity");
-const {
-    fromCognitoIdentityPool,
-} = require("@aws-sdk/credential-provider-cognito-identity");
-import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
-const REGION = 'eu-central-1';
-let s3ClientConfig = {
-    region: REGION,
-    credentials: fromCognitoIdentityPool({
-        client: new CognitoIdentityClient({ region: REGION }),
-        identityPoolId: 'eu-central-1:7779208d-624e-4741-9f76-42a18bbd9cae',
-    }),
-};
-const client = new S3Client(s3ClientConfig);
-let listBucketsCommand = new ListBucketsCommand({});
 
-function listBuckets() {
-    client.send(listBucketsCommand).then(
-        (data) => {
-            alert(data.Buckets);
-        },
-        (error) => {
-            // const { requestId, cfId, extendedRequestId } = error.$metadata;
-            // console.log({ requestId, cfId, extendedRequestId });
-            console.log('Error', error);
-        }
-    );
+const REGION = 'eu-central-1';
+const IDENTITY_POOL_ID = 'eu-central-1:25035041-4088-45af-80da-abdaf28f521b';
+const KWS_NAME = 'traffic-guard';
+const DATA_ENDPOINT = `https://grt9g38s1i.execute-api.eu-central-1.amazonaws.com/Prod/process-webcam-stream/${KWS_NAME}/`;
+
+var frameBuffer;
+
+
+function startStreaming() {
+    // Inititialize frame buffer.
+    frameBuffer = new FrameBuffer({ size: 40 });
+    // Attempt to stream webcam feed to canvas element.
+    Webcam.attach("#webcam-feed-container");
+    // When webcam feed acquired, executes callback.
+    Webcam.on('live', startStreamLoop);
 }
 
-// const listBucketsId = Symbol.for("listBuckets");
-// window[listBucketsId] = listBuckets;
-window.listBuckets = listBuckets;
+var looperPromise;
+function startStreamLoop() {
+    var TARGET_FPS = 10;
+    var looper = function() {
+        // Pass current frame image data to handler.
+        Webcam.snap(frameCallback);
+        looperPromise = setTimeout(looper, 1000 / TARGET_FPS);
+    }
+    looper();
+}
 
-window.hi = function() {
-    alert('Hi');
+function frameCallback(imgData) {
+    // imgData is base64-encoded string of current frame
+    // e.g. "data:image(jpeg|png);base64,----"; this is generated in WebcamJS library by calling
+    // canvas.toDataURL('image/jpeg')
+    frameBuffer.addFrame(imgData);
+    if (frameBuffer.getSize() >= frameBuffer.bufferSize) {
+        // Clear buffer, and post frames to data endpoint.
+        var data = frameBuffer.getData();
+        frameBuffer.clear();
+        // DATA_ENDPOINT is API endpoint that handles conversion of image frame sequence to streamable MKV fragment.
+        postFrameData(data, DATA_ENDPOINT);
+    }
+}
+
+function postFrameData(data, endpoint, callback) {
+    var $http = new XMLHttpRequest();
+    $http.open("POST", endpoint);
+    $http.setRequestHeader("Content-Type", "application/json");
+
+    $http.send(JSON.stringify(data));
+}
+
+function stopStreaming() {
+    try {
+        Webcam.reset();
+        Webcam.off('live');
+        clearTimeout(looperPromise);
+        frameBuffer.clear();
+    } catch(e) {
+        console.error(e);
+    }
 }
