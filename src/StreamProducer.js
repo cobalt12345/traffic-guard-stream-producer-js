0d772351bf6117dd4daa5c32c6288121516d5492
dtalochk <denis.talochkin@gmail.com>
2021-05-07 20:05:15 +0300

    Try to make CloudFrond script to work properly.

--- src/StreamProducer.js
+++ src/StreamProducer.js
@@ -8,7 +8,7 @@ const REGION = 'eu-central-1';
 const IDENTITY_POOL_ID = 'eu-central-1:25035041-4088-45af-80da-abdaf28f521b';
 const KVS_NAME = 'traffic-guard';
 const isInProduction = false;
-const DATA_ENDPOINT = `https://6t9eq1coo0.execute-api.eu-central-1.amazonaws.com/Stage/streams?stream=traffic-guard`;
+const DATA_ENDPOINT = `https://6t9eq1coo0.execute-api.eu-central-1.amazonaws.com/Stage/streams`;
 
 const webcamConfig = {
     /*
